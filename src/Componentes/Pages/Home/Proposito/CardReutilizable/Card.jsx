67ac8632419573cd3d70bde6385e072a6876acfa
David-ReyesPe <118202354+David-ReyesPe@users.noreply.github.com>
2023-09-14 16:40:35 -0500

    Cards de proposito

--- src/Componentes/Pages/Home/Proposito/CardReutilizable/Card.jsx
+++ src/Componentes/Pages/Home/Proposito/CardReutilizable/Card.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function Card({ imagen, descripcion, titulo }) {
     return (
-        <div className="xl:w-72 md:max-w-sm w-full rounded-lg overflow-hidden shadow-lg p-4 md:p-5">
+        <div className="xl:w-72 md:max-w-sm w-full rounded-lg overflow-hidden shadow-lg p-4 md:p-5 text-center">
             <h3 className="text-[#ffbd69] font-mono text-[28px] md:text-[32px]">{titulo}</h3>
             <h3 className="text-base md:text-lg">{descripcion}</h3>
             <img className="h-40 w-full md:w-25 p-2 md:p-3 object-contain" src={imagen} alt={descripcion} />
